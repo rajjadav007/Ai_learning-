277bc35ab4f82532e04233655c06b8621981fa5d
rajjadav <rajjadav0906@gmail.com>
2026-09-22 22:48:52 +0530

    Day 1

---
+++ Day 1/take name.py
@@ -0,0 +1,4 @@
+a=(input("enter the name"))
+b=(input("enter the age"))
+c=a,b
+print ("the person of information is",c)
---
+++ Day 1/variable.py
@@ -0,0 +1,5 @@
+name="raj"
+age=18
+
+print ("the person of name is",name)
+print ("the person of age is",age)
