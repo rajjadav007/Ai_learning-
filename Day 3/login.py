8349d072a2eb85f815a436aba1dcf7830865a33e
rajjadav <rajjadav0906@gmail.com>
2026-09-24 23:18:07 +0530

    day 3

---
+++ Day 3/login.py
@@ -0,0 +1,12 @@
+username=str(input("enter the username"))
+password=str(input("enter the username"))
+
+print ("you are sucessfully sign up ")
+
+user_name=str(input("enter the username"))
+user_pass=str(input("enter the passworkd"))
+
+if username==user_name and password==user_pass:
+    print ("you are login")
+else:
+    print ("you are not login ")
